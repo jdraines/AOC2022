2d0109e77972dd6ed548393b451bbef252acfd9b
John Raines <johndanielraines@gmail.com>
2022-12-12 18:51:47 +0000

    init day8

---
+++ days/day8/day8.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "../../utils/input.h"
+#include "day8lib.h"
+
+
+void part1(int argc, char* argv[]);
+void part2(int argc, char* argv[]);
+
+int main(int argc, char* argv[])
+{
+    part1(argc, argv);
+    //part2(argc, argv);
+    return 0;
+}
+
+void part1(int argc, char* argv[])
+{
+    aoc::InputDoc input = aoc::get_input(argc, argv);
+}
+
+void part2(int argc, char* argv[])
+{
+    aoc::InputDoc input = aoc::get_input(argc, argv);
+}
---
+++ days/day8/day8lib.h
@@ -0,0 +1,5 @@
+#ifndef DAY8LIB_H
+#define DAY8LIB_H
+
+
+#endif /* DAY8LIB_H */
